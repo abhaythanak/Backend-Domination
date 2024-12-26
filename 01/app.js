e40c69a290971672ab922ebf67393c9ee8bf666a
abhaythanak <abhaythanak@gmail.com>
2024-12-26 22:01:36 +0530

    first commit

---
+++ 01/app.js
@@ -0,0 +1,21 @@
+ const { log } = require('console');
+const fs = require('fs');
+
+ //   create file
+//  fs.writeFile("abcd.txt","hey hello",function(err) {
+//     if (err) console.log(err);
+//     else console.log("file created")
+//  })
+
+// fs.readFile("abcd.txt","utf8", function(err,data){
+//     if (err) console.log(err)
+//     else console.log(data)
+// })
+
+// fs.appendFile("abcd.txt"," appended data",function(err){
+//     if(err) console.log(err);
+//     else console.log("appended data");
+        
+// })
+
+fs.rename("abcd.txt","")
---
+++ 01/f.md
@@ -0,0 +1,14 @@
+ promise API
+ callback API
+ 
+ 
+ const fs = require('fs')
+
+ write file
+ read file
+ update file
+ append file
+ folder creation
+ delete file, copy, move,
+
+ <Buffer 68 65 79 20 68 65 6c 6c 6f> ==> hexa decimal language
