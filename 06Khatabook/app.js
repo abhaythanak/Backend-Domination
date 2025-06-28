7bd2360854c0902968c1b5352ae1859a8f0ad549
abhaythanak <abhaythanak@gmail.com>
2025-06-28 19:05:22 +0530

    creating and editing section of ejs

--- 06Khatabook/app.js
+++ 06Khatabook/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const path = require("path")
+const  fs = require("fs");
+const { send } = require("process");
 
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -8,8 +10,41 @@ app.use(express.urlencoded({ extended: true}));
 app.use(express.static(path.join(__dirname,"public")))
 
 app.get("/",(req,res)=>{
-    // res.send("hey bro ")
-    res.render("editing")
+   fs.readdir(`./hisaab`, (err, files)=>{
+    if(err) return res.status(500).send(err)
+        res.render("index",{files:files})
+   })
+    
 })
 
+app.get("/create",(req,res)=>{
+    res.render("creating")
+})
+
+app.get("/edit/:filename",(req,res)=>{
+    fs.readFile(`./hisaab/${req.params.filename}`,"utf8", (err, filedata)=>{
+        if(err) return res.status(500).send(err)
+            res.render("editing",{filedata,  filename: req.params.filename })
+       })
+   
+})
+
+app.post("/createhisaab",(req,res)=>{
+    // file name on date
+    let currentdate = new Date()
+    let date = `${currentdate.getDate()}-${currentdate.getMonth()+1}-${currentdate.getFullYear()}`
+    // res.send(req.body)
+    fs.writeFile(`./hisaab/${date}.txt`,req.body.content, (err)=>{
+        if(err) return res.status(500).send(err);
+        res.redirect("/")
+    })
+})
+
+app.post("/update/:filename",(req,res)=>{
+    fs.writeFile(`./hisaab/${req.params.filename}`, req.body.content, (err)=>{
+        if(err) return res.status(500).send(err)
+            res.redirect("/")
+       })
+   
+})
 app.listen(3000)
